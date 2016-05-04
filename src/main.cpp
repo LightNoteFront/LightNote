ec7a222399327b0032e73b24894c63c9f3c35691
LuiCat <zhtl1995@163.com>
2016-05-04 14:04:13 +0800

    修改笔记格式

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    //QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     WebRequest request("http://133.130.125.201:3000/");
     NoteList notes(&request);
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
     //*/
 
     // 测试说明：去掉注释运行一次，本地就有记录生成，然后记得把注释加回来
-    /*
+    /*/
     notes.createNote("软件项目管理", "写文档");
     notes.createNote("软件项目管理", "写代码");
     notes.createNote("软件项目管理", "做测试");
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
     notes.createNote("编译原理", "预习笔记！！！");
     notes.createNote("编译原理", "写作业");
     notes.createNote("编译原理", "张天利是个辣鸡");
-    */
+    //*/
 
     QQmlApplicationEngine engine;
     QQmlContext *context=engine.rootContext();
--- src/note.cpp
+++ src/note.cpp
@@ -1,6 +1,7 @@
 #include "note.h"
 
 #include <QJsonArray>
+#include <QJsonDocument>
 #include <QJsonObject>
 
 Note::Note(QObject *parent, int id)
@@ -25,32 +26,44 @@ Note::Note(const Note& other)
 
 void Note::read(const QJsonObject& json)
 {
-    webId = json.value("webid").toString();
-    title = json.value("title").toString();
-    genre = json.value("genre").toString();
-    authorId = json.value("author").toString();
+    webId = json.value("noteID").toString();
+    authorId = json.value("userID").toString();
+
+    QByteArray raw = QByteArray::fromBase64(json.value("content").toString().toUtf8());
+    QJsonObject objContent = QJsonDocument::fromJson(raw).object();
+
     tags.clear();
-    auto arrtag = json.value("tags").toArray();
+    auto arrtag = objContent.value("tags").toArray();
     for(auto var : arrtag)
     {
         auto tag = var.toString();
         if(!tag.isNull())
             tags.append(tag);
     }
-    content = QString::fromUtf8(QByteArray::fromBase64(json.value("content").toString().toUtf8()));
+
+    genre = objContent.value("genre").toString();
+    title = objContent.value("title").toString();
+    content = QString::fromUtf8(QByteArray::fromBase64(objContent.value("text").toString().toUtf8()));
     emit infoChanged();
 }
 
 void Note::write(QJsonObject& json) const
 {
     if(!webId.isNull())
-        json.insert("webid", webId);
-    json.insert("title", title);
-    json.insert("genre", genre);
+        json.insert("noteID", webId);
     if(!authorId.isNull())
-        json.insert("author", authorId);
-    json.insert("tags", QJsonArray::fromStringList(tags));
-    json.insert("content", QString::fromUtf8(content.toUtf8().toBase64()));
+        json.insert("userID", authorId);
+    QJsonArray arrtag = QJsonArray::fromStringList(tags);
+    json.insert("tags", arrtag);
+
+    QJsonObject objContent;
+    objContent.insert("tags", arrtag);
+    objContent.insert("genre", genre);
+    objContent.insert("title", title);
+    objContent.insert("text", QString::fromUtf8(content.toUtf8().toBase64()));
+    QJsonDocument docContent(objContent);
+    //qDebug() << docContent;
+    json.insert("content", QString::fromUtf8(docContent.toJson().toBase64()));
 }
 
 Note& Note::operator=(const Note& other)
